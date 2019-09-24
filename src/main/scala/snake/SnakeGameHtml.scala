ca9e816ba04427ff257712acdad89d5fdfddb87d
Bharg Sharma <bharg.sharma@rea-group.com>
2019-09-24 17:04:59 +1000

    Add snake, wall and direction for snake

--- src/main/scala/snake/SnakeGameHtml.scala
+++ src/main/scala/snake/SnakeGameHtml.scala
@@ -6,11 +6,14 @@ class SnakeGameHtml(document: Document) {
   def render(snakeGameWorld: SnakeGameWorld): Node = {
     val board = document.createElement("div")
     board.classList.add("board")
+    val newSnakeGameWorld = putSnakeOn(snakeGameWorld)
     Range(0, snakeGameWorld.board.height).map { y =>
       Range(0, snakeGameWorld.board.width).map { x =>
         val cell = document.createElement("div")
-        cell.setAttribute("style", s"grid-column: $x; grid-row: $y;")
-        val text = document.createTextNode(snakeGameWorld.board.cell(x + y * snakeGameWorld.board.width).toString)
+        cell.setAttribute("style", s"grid-column: ${x+1}; grid-row: ${snakeGameWorld.board.height - y};")
+        val text = document.createTextNode(
+          newSnakeGameWorld.board.cell(cellIndex(newSnakeGameWorld, x, y)).toString
+        )
         cell.appendChild(text)
         board.appendChild(cell)
       }
@@ -19,6 +22,19 @@ class SnakeGameHtml(document: Document) {
     board
   }
 
-//TODO: add snake onto board
-  // TODO: add a boundary / wall
+  private def cellIndex(snakeGameWorld: SnakeGameWorld, x: Int, y: Int) = {
+    x + y * snakeGameWorld.board.width
+  }
+
+  def putSnakeOn(snakeGameWorld: SnakeGameWorld): SnakeGameWorld = {
+    val currentSnakeLocation = snakeGameWorld.snake.location.map {
+      case Location(x, y) => cellIndex(snakeGameWorld, x, y)
+    }
+    val newCells = snakeGameWorld.board.cell.zipWithIndex.map{
+      case (cell, index) => if(currentSnakeLocation.contains(index)) SnakePart else cell
+    }
+    snakeGameWorld.copy(board = snakeGameWorld.board.copy(cell = newCells))
+
+  }
 }
+//TODO: make the snake move, read from the keys
--- src/main/scala/snake/SnakeGameWorld.scala
+++ src/main/scala/snake/SnakeGameWorld.scala
@@ -4,7 +4,7 @@ case class SnakeGameWorld(snake: Snake, board: Board, food: Option[Food]) {
 
 }
 
-case class Snake(location: Location, length: Int)
+case class Snake(location: List[Location], length: Int, direction: Direction)
 case class Location(x: Int, y: Int)
 case class Board(cell: Array[Cell], width: Int, height: Int)
 sealed trait Cell
@@ -12,13 +12,27 @@ case object SnakePart extends Cell
 case object Wall extends Cell
 case object EmptyCell extends Cell
 case class Food(location: Location) //expiry time
+sealed trait Direction
+case object Up extends Direction
+case object Down extends Direction
+case object Left extends Direction
+case object Right extends Direction
 
 object SnakeGameWorld {
-  private val emptyCells: Array[Cell] = Array.fill(100)(EmptyCell)
+  private val emptyCells: Array[Cell] = Array.tabulate(100){ index =>
+    if (index % 10 == 0 || index % 10 == 9 ) {
+      Wall
+    }
+    else if (index / 10 == 0 || index / 10 == 9){
+      Wall
+    }
+    else {
+      EmptyCell
+    }
+  }
   val board = Board(emptyCells, 10, 10)
 
-  private val location = Location(5, 5)
-  private val snake = Snake(location, 2)
+  private val snake = Snake(List(Location(5, 5), Location(5,4)), 2, Up)
 
   val food: Option[Food] = None
 
