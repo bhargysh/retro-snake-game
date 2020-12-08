8d1cb3104cf4b622dd320ff6e005ffed458e1839
Bharg Sharma <bsha492@aucklanduni.ac.nz>
2020-12-08 14:35:34 +1100

    Introduce IO to side effects in Main

--- src/main/scala/snake/Main.scala
+++ src/main/scala/snake/Main.scala
@@ -1,20 +1,32 @@
 package snake
+import cats.effect.{ExitCode, IO, IOApp, Timer}
 import org.scalajs.dom
-import dom.{Node, document, window}
+import dom.{Node, console, document, window}
 import org.scalajs.dom.raw.{Element, KeyboardEvent}
 
+import scala.concurrent.duration.{Duration, SECONDS}
 import scala.scalajs.js.annotation.{JSExport, JSExportTopLevel}
 
-object Main {
-  def main(args: Array[String]): Unit = {
-    appendPar(document.body, "Snake game!!!!")
-    val world = SnakeGameWorld.newSnakeGameWorld
-    val html = new SnakeGameHtml(document)
-    val renderedWorld: Node = html.render(world)
-    val boardUI: Element = document.createElement("div")
-    document.body.appendChild(boardUI)
-    boardUI.appendChild(renderedWorld)
-    window.setTimeout(() => updateGame(world, html, renderedWorld, boardUI), 1000)
+object Main extends IOApp {
+  def run(args: List[String]): IO[ExitCode] =
+    for {
+      world <- IO.pure(SnakeGameWorld.newSnakeGameWorld)
+      html = new SnakeGameHtml(document)
+      renderedWorld: Node = html.render(world)
+      boardUI <- appendBoardToDocument(renderedWorld)
+      _ <- loop[Node](updateGame(world, html, boardUI))(renderedWorld)
+      _ <- actionOnKeyboardEvent(boardUI)
+    } yield ExitCode.Success
+
+  private def loop[A](work: A => IO[A])(oldRenderedNode: A): IO[Nothing] = for {
+    _ <- timer.sleep(Duration(1, SECONDS))
+    _ = IO(console.log("-------", oldRenderedNode))
+    newRenderedNode <- work(oldRenderedNode)
+    _ = IO(console.log("-------", newRenderedNode)) //TODO: IO is not evaluating, work out why!
+    result <- loop(work)(newRenderedNode)
+  } yield result
+
+  private def actionOnKeyboardEvent(boardUI: Element): IO[Unit] = IO {
     document.addEventListener("keydown", (event: KeyboardEvent) => {
       val maybeDirection = Keyboard.stringToDirection(event.key)
       maybeDirection match {
@@ -24,23 +36,25 @@ object Main {
     })
   }
 
-  private def updateGame(world: SnakeGameWorld, html: SnakeGameHtml, oldWorld: Node, boardUI: Element): Unit = {
-    val maybeDirectionData: Option[String] = Option(boardUI.getAttribute("data-direction"))
-    val maybeDirection = maybeDirectionData.flatMap(Direction.fromStr)
-    val newWorld = world.play(maybeDirection)
-    val newRenderedWorld = html.render(newWorld)
-    boardUI.replaceChild(newRenderedWorld, oldWorld)
-    if (newWorld.isPlaying) {
-      window.setTimeout(() => updateGame(newWorld, html, newRenderedWorld, boardUI), 1000)
-    }
+//  private def setTimeout(world: SnakeGameWorld, html: SnakeGameHtml, renderedWorld: Node, boardUI: Element): IO[Int] = IO {
+//    window.setTimeout(() => updateGame(world, html, renderedWorld, boardUI), 1000)
+//  }
+
+  private def appendBoardToDocument(renderedWorld: Node): IO[Element] = IO {
+    val boardUI: Element = document.createElement("div")
+    document.body.appendChild(boardUI)
+    boardUI.appendChild(renderedWorld)
+    boardUI
   }
 
-  def appendPar(targetNode: dom.Node, text: String): Unit = {
-    val parNode = document.createElement("p")
-    val textNode = document.createTextNode(text)
-    parNode.appendChild(textNode)
-    targetNode.appendChild(parNode)
-  }
+  private def updateGame(world: SnakeGameWorld, html: SnakeGameHtml, boardUI: Element)(oldWorld: Node): IO[Node] = for {
+    maybeDirectionData <- IO(Option(boardUI.getAttribute("data-direction")))
+    maybeDirection = maybeDirectionData.flatMap(Direction.fromStr)
+    newWorld = world.play(maybeDirection)
+    newRenderedWorld = html.render(newWorld)
+    _ <- IO(boardUI.replaceChild(newRenderedWorld, oldWorld))
+//    y = if (newWorld.isPlaying) {
+//      setTimeout(newWorld, html, newRenderedWorld, boardUI)
+//    }
+  } yield newRenderedWorld
 }
-
-//TODO: Dec 1st could we use IO in this?
