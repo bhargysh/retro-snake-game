4f152189321bc081514e98f02250ab4fa25ce206
Bharg Sharma <bsha492@aucklanduni.ac.nz>
2021-04-01 10:22:40 +1100

    Fully implement obstacle generator

--- src/main/scala/snake/ObstacleGenerator.scala
+++ src/main/scala/snake/ObstacleGenerator.scala
@@ -3,16 +3,21 @@ package snake
 import scala.util.Random
 
 trait ObstacleGenerator {
-  def apply(moveNumber: MoveNumber, snake: Snake, board: Board): Location
+  def apply(food: Food, snake: Snake, board: Board): Location
 }
 
 
 class RandomObstacleGenerator(randomNumberGenerator: Random) extends ObstacleGenerator {
-  def apply(moveNumber: MoveNumber, snake: Snake, board: Board): Location = {
-    val notAvailable: Set[Location] = snake.location.toSet
+  def apply(food: Food, snake: Snake, board: Board): Location = {
+    val snakeLocation: Set[Location] = snake.location.toSet
     val emptyLocations: Set[Location] = board.locations.filter { location =>
       board.cellAt(location) == EmptyCell
     }
+    val foodLocation: Set[Location] = food match {
+      case FoodPresent(location, _) => Set(location)
+      case FoodAbsent(_) => Set.empty[Location]
+    }
+    val notAvailable = snakeLocation ++ foodLocation
     val availableLocations = (emptyLocations -- notAvailable).toVector
     val position = randomNumberGenerator.nextInt(availableLocations.size)
     availableLocations(position)
--- src/test/scala/snake/RandomObstacleGeneratorSpec.scala
+++ src/test/scala/snake/RandomObstacleGeneratorSpec.scala
@@ -4,6 +4,7 @@ import org.specs2.ScalaCheck
 import org.specs2.mutable.Specification
 import Generators._
 import org.scalacheck.{Arbitrary, Gen}
+import org.specs2.scalacheck.Parameters
 
 import scala.util.Random
 
@@ -19,15 +20,12 @@ class RandomObstacleGeneratorSpec extends Specification with ScalaCheck {
     } yield Board(cells.toArray, width, height)
     implicit val board: Arbitrary[Board] = Arbitrary(boardGen)
 
-    "generate obstacle that is not on the snake, food or wall" >> prop { (moveNumber: MoveNumber, snake: Snake, food: FoodPresent, board: Board) =>
+    "generate obstacle that is not on the snake, food or wall" >> prop { (snake: Snake, food: FoodPresent, board: Board) =>
       val obstacleGenerator = new RandomObstacleGenerator(new Random)
-      val result: Location = obstacleGenerator.apply(moveNumber, snake, board)
-      result should beLike[Location]{ case Location(x, y) =>
-        snake.location should not(contain(result))
-        food.location shouldNotEqual result //TODO: fix generator, result should be on left comparison
-        board.cellAt(result) shouldEqual EmptyCell
-
-      }
-    }
+      val result: Location = obstacleGenerator.apply(food, snake, board)
+      snake.location should not(contain(result))
+      result shouldNotEqual food.location
+      board.cellAt(result) shouldEqual EmptyCell
+    }.setParameters(Parameters(minTestsOk = 400))
   }
 }
